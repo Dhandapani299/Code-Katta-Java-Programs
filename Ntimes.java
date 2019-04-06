eda7984ec9928e70b525c8a0ddf49ac0e0f4b8d1
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2019-04-06 10:21:07 -0700

    Add files via upload

---
+++ Ntimes.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Ntimes{
+public static void main(String args[])
+{
+
+
+System.out.println("enter the times");
+Scanner s = new Scanner(System.in);
+int i,n;
+n= s.nextInt();
+String num="hello";
+
+for(i=0;i <n;i++)
+{
+
+System.out.println(num);
+}
+
+
+}
+}
